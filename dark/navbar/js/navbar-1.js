a577e54f36add22c5c2880461b2eadaf2c4d64cb
apriliandi <apriliandi642@gmail.com>
2020-09-05 14:14:46 +0700

    factory cod

--- dark/navbar/js/navbar-1.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-document.querySelector('.menu-toggle input').addEventListener('click', () => {
-   document.querySelector('nav ul').classList.toggle('slide');
-=======
-document.querySelector('.toggle-menu').addEventListener('click', () => {
-   document.querySelector('.navbar__items').classList.toggle('navbar__items--slide');
->>>>>>> change-class-name-structure
-});
