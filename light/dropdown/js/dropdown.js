7bffc2a1674f1e8c23e016c436e19aa6baf356d6
apriliandi <apriliandi642@gmail.com>
2020-09-21 00:46:59 +0700

    change path js file in light component

--- light/dropdown/js/dropdown.js
+++
@@ -1,9 +0,0 @@
-const dropdownContent = document.querySelector('.dropdown__content');
-
-document.querySelector('.dropdown__button').addEventListener('click', () => {
-   dropdownContent.style.display === 'block' ? setDisplayValue('none') : setDisplayValue('block');
-});
-
-function setDisplayValue(value) {
-   dropdownContent.style.display = value;
-}
